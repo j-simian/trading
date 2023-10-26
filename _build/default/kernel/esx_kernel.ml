474dd2fee1283a5d9d2a2577a02dbc9b468ba80a
Jake Simian <jake.simian@keble.ox.ac.uk>
2023-10-26 18:11:42 +0100

    finish on desktop

--- _build/default/kernel/esx_kernel.ml
+++ _build/default/kernel/esx_kernel.ml
@@ -2,7 +2,13 @@ open! Core
 open! Async
 
 module Message = struct
-    type t = 
-        | Handshake_initiate of string
+  module Message_type = struct
+    type t = Handshake_initiate of string 
     [@@deriving bin_io, sexp]
+  end
+
+  type t = 
+    { message_id: int;
+      message_type: Message_type.t} 
+  [@@deriving bin_io, sexp]
 end
--- _build/default/kernel/esx_kernel.mli
+++ _build/default/kernel/esx_kernel.mli
@@ -2,7 +2,14 @@ open! Core
 open! Async
 
 module Message : sig 
+  module Message_type : sig
     type t = 
-        | Handshake_initiate of string
+      | Handshake_initiate of string
     [@@deriving bin_io, sexp]
+  end
+
+  type t =  
+    { message_id : int
+    ; message_type : Message_type.t }
+  [@@deriving bin_io, sexp]
 end
